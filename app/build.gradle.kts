abeb6b7a3edf5fda894ba8e02928ceb4c9ed617d
HAND33h <uhgeli@gmail.com>
2026-09-08 14:52:56 +0300

    Build Firebase-free install-test APK with fresh applicationId

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("com.google.gms.google-services")
 }
 
 android {
@@ -33,6 +32,4 @@ android {
 }
 
 dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
-    implementation("com.google.firebase:firebase-analytics")
 }
