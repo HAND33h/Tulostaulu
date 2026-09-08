f391d7eaf3bc5f8f2e4aae2ee9d4e36fff18c7f1
HAND33h <uhgeli@gmail.com>
2026-09-08 12:45:39 +0300

    Bump Bunny King to v2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.hand33h.tulostaulu"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "2.3"
+        versionCode = 15
+        versionName = "2.4"
     }
 
     buildTypes {
